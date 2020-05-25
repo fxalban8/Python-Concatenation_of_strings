0ac30dc60ec336dd39b72d27531346c33fce38bd
fxalban8 <francisco.alban@epn.edu.ec>
2020-05-25 11:22:30 -0500

    It's been added my college's name

--- concatenation_of_string_variables.py
+++ concatenation_of_string_variables.py
@@ -1,5 +1,9 @@
 string1= "Francisco"
 string2= "Albán"
 string3= " "
+string4= "Escuela"
+string5= "Politécnica"
+string6= "Nacional"
 
-print(string1+" "+ string2)
+
+print(string1+" "+ string2+ " -"+ string4+ " "+string5+ " "+ string6)
